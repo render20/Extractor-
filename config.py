c7c7f9a3cc63014aa43a2afd0dd0d3d1619b469c
render20 <rendergithub20@gmail.com>
2025-09-19 20:50:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "enter your api id here"
 api_hash = "enter your api hash here"
 bot_token = "enter your bot token here"
-auth_users = [enter your auth user id here]
+auth_users = [8075836012]
